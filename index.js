45275016f7db6b31940da106421a2f17b162c172
kaelzhang <i@kael.me>
2014-08-20 01:08:45 +0800

    complete arguments parsing;

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ function parse (code, options) {
   index = -1;
   next();
 
-  // var top_comment;
-  // var bottom_comment;
-  // if (is('comment')) {
-  //   top_comment = current;
-  // }
-
   var result = walk();
+
+  if (Object(result) === result) {
+    result['//^'] = tokens.head_comments;
+    result['//$'] = tokens.foot_comments;
+  }
+
   return result;
 }
 
@@ -136,5 +136,100 @@ function is (t) {
 
 
 function sort_comment_tokens () {
+  var ts = tokens;
+  var comments = ts.comments;
+  if (!comments) {
+    return;
+  }
+
+  function compare_to_then_push (condition, to, setup) {
+    var comment;
+    var first = true;
+    var host;
+    while((comment = comments[ci ++]) && condition(comment, to)){
+      if (first) {
+        host = setup();
+      }
+      first = false;
+      host.push(comment_content(comment));
+    }
+
+    ci --;
+    // Whether there are comments left.
+    return !!comment;
+  }
+  
+  var head_comments = [];
+  var foot_comments = [];
+
+  var first = ts[0];
+  var ci = 0;
+  var comment = compare_to_then_push(left, first, function () {
+    return head_comments;
+  });
+
+  var i = 0;
+  var token;
+  var next;
+  for (; i < ts.length; i ++) {
+    if (!comment) {
+      break;
+    }
+
+    token = ts[i];
+    next = ts[i + 1];
+
+    if (token.type === 'String' && next && next.value === ':') {
+      comment = compare_to_then_push(left, token, function () {
+        token.comments || (token.comments = []);
+        return token.comments[0] || (token.comments[0] = []);
+      });
+
+      if (!comment) {
+        break;
+      }
+
+      comment = compare_to_then_push(right, token, function () {
+        token.comments || (token.comments = []);
+        return token.comments[1] || (token.comments[1] = []);
+      });
+    }
+  }
+
+  compare_to_then_push(function () {
+    return true
+  }, null, function () {
+    return foot_comments;
+  });
+
+  comments.length = 0;
+  delete ts.comments;
+
+  tokens.head_comments = head_comments;
+  tokens.foot_comments = foot_comments;
 }
 
+
+function left (a, b) {
+  return a 
+    && (
+      a.loc.start.line < b.loc.start.line
+      ||
+        a.loc.start.line === b.loc.start.line
+        && a.loc.start.column < b.loc.start.column
+    );
+}
+
+
+function right (a, b) {
+  return a 
+    && a.loc.start.line === b.loc.start.line
+    && a.loc.start.column > b.loc.start.column;
+}
+
+
+function comment_content (comment) {
+  return comment.type === 'Block'
+    ? '/*' + comment.value + '*/'
+    : '//' + comment.value;
+}
