b882cd97f3f6fb0e08fe76f382ad345210a49af8
Ariya Hidayat <ariya.hidayat@gmail.com>
2015-11-06 21:07:40 -0800

    Reconstruct the comments array if it is not supplied.

--- index.js
+++ index.js
@@ -7,10 +7,29 @@ exports.parse = parse;
 
 
 function tokenize (code) {
-  return esprima.tokenize(code, {
+  var list = esprima.tokenize(code, {
     comment: true,
     loc: true
   });
+  if (list.comment) {
+    return list;
+  }
+
+  var result = [];
+  var comments = [];
+  list.map(function (t) {
+    if (t.type === 'LineComment') {
+      t.type = 'Line';
+      comments.push(t);
+    } else if (t.type === 'BlockComment') {
+      t.type = 'Block';
+      comments.push(t);
+    } else {
+      result.push(t);
+    }
+  });
+  result.comments = comments;
+  return result;
 }
 
 var tokens;
